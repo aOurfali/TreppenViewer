8ccff8e2366227fef454d2a164f147076dff6544
Jessie Zeukeng <Jessie@cs.local>
2021-05-05 15:35:57 +0200

    new marker added

--- abstand.js
+++ abstand.js
@@ -1,205 +1,108 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
 import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/controls/OrbitControls.js';
 import {OBJLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/OBJLoader.js';
+import {STLLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/STLLoader.js';
 import {MTLLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/MTLLoader.js';
-import {Detector} from 'https://dl.dropboxusercontent.com/s/ddt89ncslm4o7ie/Detector.js'
-import {TGALoader} from 'https://dl.dropboxusercontent.com/s/n5sjyymajykna51/TGALoader.js'
-import {STLLoader} from 'https://dl.dropboxusercontent.com/s/h18h48v52739df4/STLLoader.js'
+var objs = [];
 
-var container;
-var camera, controls, scene, renderer, model;
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.setScalar(7);
+var renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-init();
-animate();
+var controls = new OrbitControls(camera, renderer.domElement);
 
-function init() {
-    //container = document.createElement('div');
-    //document.body.appendChild(container);
-    const canvas = document.getElementById('webgl');
+var loader = new STLLoader();
+loader.load('https://threejs.org/examples/models/stl/ascii/slotted_disk.stl', function(geometry) {
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
+  var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: "gray"}) );
+  mesh.rotation.set(0, Math.PI, 0);
+  mesh.scale.setScalar(10);
+  scene.add(mesh);
+  objs.push(mesh);
+  
+  var wireframe = new THREE.LineSegments(new THREE.WireframeGeometry(geometry), new THREE.LineBasicMaterial({color: "aqua"}));
+  mesh.add(wireframe);
 
-    controls = new THREE.OrbitControls(camera);
+});
 
-    // scene
-    scene = new THREE.Scene();
+//load von OBj datei.
 
-    var ambient = new THREE.AmbientLight(0x404040); //0x101030
-    scene.add(ambient);
-
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    directionalLight.position.set(1, 1, 1).normalize();
-    scene.add(directionalLight);
-
-    // Loading manager
-    var manager = new THREE.LoadingManager();
-    manager.onProgress = function (item, loaded, total) {
-        console.log(item, loaded, total);
-    };
-
-    var onProgress = function (xhr) {
-        if (xhr.lengthComputable) {
-            var percentComplete = xhr.loaded / xhr.total * 100;
-            console.log(Math.round(percentComplete, 2) + '% downloaded');
-        }
-    };
-
-    var onError = function (xhr) {
-        console.log('Error: ' + xhr);
-    };
-
-    // Model
-    //model = new THREE.Object3D();
-    //scene.add(model);
-    
-    var material = new THREE.MeshPhongMaterial({
-        color: 0xffffff,
-        needsUpdate: true
+const mtlLoader = new MTLLoader();
+    mtlLoader.load('/image/hologram.mtl', (mtl) => {
+      mtl.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(mtl);
+      objLoader.load('/image/hologram.obj', (root) => {
+        root.traverse( function ( child ) {
+          if ( child.isMesh ) {
+            var wireframeGeomtry = new THREE.WireframeGeometry( child.geometry );
+            var wireframeMaterial = new THREE.LineBasicMaterial( { color: 0xffffff } );
+            var wireframe = new THREE.LineSegments( wireframeGeomtry, wireframeMaterial );
+            objs.push(child);
+            child.add(wireframe);
+          }
+        } );
+        scene.add(root);
+      });
     });
 
-	window.model ='';
 
-	
-	
-	// Added an if else here, STL code:
-	
-	/*var loader = new THREE.STLLoader();
-				loader.load( 'https://dl.dropboxusercontent.com/s/t57h7xketafodui/5a9b75e521aaf-DI_PIPE_FBG_holder.stl', function ( geometry ) {
+var marker = new THREE.Mesh(new THREE.SphereBufferGeometry(0.25, 4, 2), new THREE.MeshBasicMaterial({
+  color: 0xFFc8FF
+}));
+marker.position.setScalar(1000);
+scene.add(marker);
 
-					var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200 } );
-					var mesh = new THREE.Mesh( geometry, material );
+var intscs = [];
+var rc = new THREE.Raycaster();
+var m = new THREE.Vector2();
+var poi = new THREE.Vector3();
+var pos = new THREE.Vector3();
+var tp = [
+  new THREE.Vector3(),
+  new THREE.Vector3(),
+  new THREE.Vector3()
+];
+var tri = new THREE.Triangle();
+var bc = new THREE.Vector3();
+var idx = 0;
 
-					mesh.position.set( 0, - 0.25, 0.6 );
-					mesh.rotation.set( 0, - Math.PI / 2, 0 );
-					mesh.scale.set( 0.05, 0.05, 0.05 );
+renderer.domElement.addEventListener("mousemove", onMouseMove);
 
-					mesh.castShadow = true;
-					mesh.receiveShadow = true;
-
-					scene.add( mesh );
-					
-				window.model = scene;
-
-				} ); 
-	*/
-	
-		
-    // This OBJ Loader will load if user requests an OBJ file. 
-	     var loader = new THREE.OBJLoader(manager);
-    var object = loader.load('/image/windmill.obj', function (Object) {
-
-        Object.castShadow = true;
-        Object.position.x = 0;
-        Object.position.y = -1;
-        Object.position.z = 0;
-
-        Object.traverse(function (child) {
-            if (child instanceof THREE.Mesh) {
-                child.material = material;
-            }
-        });
-        console.log(Object);
-				window.model = Object;
-        scene.add(Object);
-    }, onProgress, onError); 
-	    // Object
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
-
-    document.addEventListener('mousedown', onDocumentMouseDown, false);
-    window.addEventListener('resize', onWindowResize, false);
-}
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-// Measurement Code
-
-var pointA = new THREE.Vector3( 0, 1, 0 );
-var pointB = new THREE.Vector3();
-
-var markerA = new THREE.Mesh( new THREE.SphereGeometry( 0.1, 16, 16 ), new THREE.MeshBasicMaterial( { color: 0xFF5555, depthTest: false, depthWrite: false } ) );
-var markerB = markerA.clone();
-scene.add( markerA );
-scene.add( markerB );
-
-var line;
-
-function getIntersections( event ) {
-  var vector = new THREE.Vector2();
-
-  vector.set(
-    ( event.clientX / window.innerWidth ) * 2 - 1,
-    - ( event.clientY / window.innerHeight ) * 2 + 1 );
-
-  var raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera( vector, camera );	
-
-  var intersects = raycaster.intersectObjects( window.model.children );
-  
-  return intersects;
-  
-}
-
-function getLine( vectorA, vectorB ) {
-
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push( vectorA );
-  geometry.vertices.push( vectorB );
-  var material = new THREE.LineBasicMaterial({
-    color: 0xFFFF00,
-    depthWrite: false,
-    depthTest: false
-  });
-  line = new THREE.Line( geometry, material );
-  return line;
-  
-}
-
-function onDocumentMouseDown( event ) {
-
-  var intersects = getIntersections( event );
-
-  if( intersects.length > 0 ){
-
-    if ( ! pointB.equals( pointA ) ) {
-      pointB = intersects[ 0 ].point;
-    } else {
-      pointB = pointA;
-    }
-    pointA = intersects[ 0 ].point;
-    markerA.position.copy( pointA );
-    markerB.position.copy( pointB );
-    
-    var distance = pointA.distanceTo( pointB );
-    
-    if ( line instanceof THREE.Line ) {
-      scene.remove( line );
-    }
-    if ( distance > 0 ) {
-      console.log( "distance", distance );
-      alert( "distance: "+distance );
-			line = getLine( pointA, pointB );
-      scene.add(line);
-    }
-
+function onMouseMove(event) {
+  m.x = (event.clientX / window.innerWidth) * 2 - 1;
+  m.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  rc.setFromCamera(m, camera);
+  intscs = rc.intersectObjects(objs);
+  if (intscs.length > 0) {
+    let o = intscs[0];
+    poi.copy(o.point);
+    o.object.worldToLocal(poi);
+    setPos(o.faceIndex);
+    o.object.localToWorld(pos);
+    marker.position.copy(pos);
   }
-
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    render();
+function setPos(faceIndex) {
+  tp[0].fromBufferAttribute(intscs[0].object.geometry.attributes.position, faceIndex * 3 + 0);
+  tp[1].fromBufferAttribute(intscs[0].object.geometry.attributes.position, faceIndex * 3 + 1);
+  tp[2].fromBufferAttribute(intscs[0].object.geometry.attributes.position, faceIndex * 3 + 2);
+  tri.set(tp[0], tp[1], tp[2]);
+  tri.getBarycoord(poi, bc);
+  if (bc.x > bc.y && bc.x > bc.z) {
+    idx = 0;
+  } else if (bc.y > bc.x && bc.y > bc.z) {
+    idx = 1;
+  } else if (bc.z > bc.x && bc.z > bc.y) {
+    idx = 2;
+  }
+  pos.copy(tp[idx]);
 }
 
-function render() {
-    renderer.render(scene, camera);
-}
+renderer.setAnimationLoop(() => {
+  renderer.render(scene, camera);
+});
--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ function main() {
   const mouse = new THREE.Vector2();
   
   var objs=[];
-  var intscs = [];
+  var tempvertex = new THREE.Vector3();
   var poi = new THREE.Vector3();
   var pos = new THREE.Vector3();
   var tp = [
@@ -24,9 +24,14 @@ function main() {
   var bc = new THREE.Vector3();
   var idx = 0;
   
-  //const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight);
+const material= new THREE.MeshNormalMaterial()
+
+const coneGeometry = new THREE.ConeGeometry(.05, .2, 8);
+
+  
+  const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight);
  //var camera = new THREE.OrthographicCamera(0, window.innerWidth, -window.innerHeight, 0, -100, 100);
-  var camera =  new THREE.OrthographicCamera(window.innerWidth / -1, window.innerWidth /4, window.innerHeight / 2, window.innerHeight / -3, -1000, 10000);
+  //var camera =  new THREE.OrthographicCamera(window.innerWidth / -1, window.innerWidth /4, window.innerHeight / 2, window.innerHeight / -3, -1000, 10000);
   camera.position.setScalar(10);
   camera.position.set(0.5, 0.5, 2);
   camera.lookAt(0,0,0);
@@ -43,138 +48,59 @@ function main() {
   scene.add(light);
 
   // richtige Laden von OBJ-Datei
-  /*{
-    const mtlLoader = new MTLLoader();
-    mtlLoader.load('/image/hologram.mtl', (mtl) => {
-      mtl.preload();
-     // mtl.materials.default.map.magFilter = THREE.NearestFilter;
-     // mtl.materials.default.map.minFilter = THREE.LinearFilter;
-
-      const objLoader = new OBJLoader();
-      objLoader.setMaterials(mtl);
-      objLoader.load('/image/hologram.obj', (root) => {
-        root.traverse( function ( child ) {
-
-          if ( child.isMesh ) {
-        
-            var wireframeGeomtry = new THREE.WireframeGeometry( child.geometry );
-            var wireframeMaterial = new THREE.LineBasicMaterial( { color: 0xffffff } );
-            var wireframe = new THREE.LineSegments( wireframeGeomtry, wireframeMaterial );
-            child.add(wireframe);
-        
-          }
-          
-        } );
-        scene.add(root);
-      });
-    });
-  }*/
-  
-
   {
     const mtlLoader = new MTLLoader();
     mtlLoader.load('/image/hologram.mtl', (mtl) => {
       mtl.preload();
       const objLoader = new OBJLoader();
       objLoader.setMaterials(mtl);
-      objLoader.load('/image/hologram.obj', function(root) {
-        /*var material = new THREE.MeshBasicMaterial({
-          color: "gray"
-        });
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    //var mesh = new THREE.Mesh(geometry, material);
-    /*mesh.rotation.set(0, Math.PI, 0);
-    mesh.scale.setScalar(10);*/
-    //root.rotation.set(0, Math.PI, 0);
-    root.scale.setScalar(1);
-    scene.add(root);
-    //objs.push(mesh);
-    root.traverse( function ( child ) {
+      objLoader.load('/image/hologram.obj', (root) => {
+        root.traverse( function ( child ) {
 
-      if ( child.isMesh ) {
-    
-        var wireframeGeomtry = new THREE.WireframeGeometry( child.geometry );
-        var wireframeMaterial = new THREE.LineBasicMaterial( { color: 0xffffff } );
-        var wireframe = new THREE.LineSegments( wireframeGeomtry, wireframeMaterial );
-        child.add(wireframe);
-    
-      }
-      
-    } );
+          if ( child.isMesh ) {
+            var wireframeGeomtry = new THREE.WireframeGeometry( child.geometry );
+            var wireframeMaterial = new THREE.LineBasicMaterial( { color: 0xffffff } );
+            var wireframe = new THREE.LineSegments( wireframeGeomtry, wireframeMaterial );
+            objs.push(child);
+            child.add(wireframe);
+            scene.add(child);
+          }
+        } );
+      });
     });
-
-   });
-   //marker setzen
-   var marker = new THREE.Mesh(new THREE.SphereBufferGeometry(0.25, 4, 2), new THREE.MeshBasicMaterial({
-    color: 0xFFc8FF
-   }));
- // marker.position.setScalar(1000);
-  scene.add(marker);
   }
-
-  function getMousePosition(clientX, clientY) {
-    var mouse2D = new THREE.Vector3();
-    var mouse3D = new THREE.Vector3();
-    mouse2D.x = (clientX / window.innerWidth) * 2 - 1;
-    mouse2D.y = -(clientY / window.innerHeight) * 2 + 1;
-    mouse2D.z = 0.5;
-    mouse3D = camera.unprojectVector(mouse2D.clone(), camera);
-    return mouse3D;
-   
-}
-  function onDocumentMouseUp(event) {
-    event.preventDefault();
-
-    var mouse3D = getMousePosition(event.clientX, event.clientY);
-    console.log(mouse3D.x + ' ' + mouse3D.y + ' ' + mouse3D.z);
-}
+  var marker = new THREE.Mesh(new THREE.SphereBufferGeometry(0.1, 16, 16), new THREE.MeshBasicMaterial({
+    color: 0xFF5555
+   })); 
   
-  function onMouseMove(event){
 
-    event.preventDefault();
-    
-    mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
-    console.log("die werte von Mouse.x ist :" + mouse.x)
-    raycaster.setFromCamera( mouse, camera );
-    
-    var intersects = 0;//raycaster.intersectObjects( objects);
-    if (intersects.length > 0)
-        return intersects[0].point;
-  }
-  function onMouseMove1( event ) {
+  //marker setzen
 
-    // Get screen-space x/y
-    mouse.x = ( event.clientX / renderer.domElement.clientWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / renderer.domElement.clientHeight ) * 2 + 1;
-
-    // Perform raycast
-    raycaster.setFromCamera( mouse, camera );
-
-    // See if the ray from the camera into the world hits our mesh
-    const intersects = raycaster.intersectObjects( scene.children , true);
-
-    // Check if an intersection took place
-    if ( intersects.length > 0 ) {
-        const posX = intersects[0].point.x;
-        const posZ = intersects[0].point.z;
-        console.log(posX, posZ);
-    }
-
-}
 function raycast ( e ) {
 
       mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
       mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
   
       raycaster.setFromCamera( mouse, camera );    
-      var intersects = raycaster.intersectObjects( scene.children, true );
-      
-      //rollOverMesh.material.color = new THREE.Color(0xffffff * Math.random());
-      //rollOverMesh.material.needsUpdate = true;
+      var intersects = raycaster.intersectObjects( objs, true );
+      if (intersects.length > 0){
+        console.log( intersects[0].face ); 
+          let n = new THREE.Vector3();
+          n.copy(intersects[0].face.normal);
+          n.transformDirection(intersects[0].object.matrixWorld); 
+          marker.lookAt(n);
+          marker.rotateX(Math.PI / 2)
+          marker.position.copy(intersects[0].point);
+          marker.position.addScaledVector(n, 0.1);
+          scene.add(marker);
+          //objs.push(cube);
 
-      for ( var i = 0; i < intersects.length; i++ ) {
-          console.log( intersects[ i ] ); 
+         /* let raycasterp= intersects[i] ;
+          let pIntersect = raycasterp.point.clone();
+          raycasterp.object.worldToLocal(pIntersect);
+         // raycast.object.localToWorld(raycasterp);
+         // marker.position.copy(raycasterp.face.normal).mutiplyScalar(0.25).add(pIntersect);
+          //objs.add(marker);
           /*
               An intersection has the following properties :
                   - object : intersected object (THREE.Mesh)
@@ -184,65 +110,13 @@ function raycast ( e ) {
                   - point : intersection point (THREE.Vector3)
                   - uv : intersection point in the object's UV coordinates (THREE.Vector2)
           */
-      }
-   /*   mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-      mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-  
-      raycaster.setFromCamera( mouse, camera );    
-  
-      var intersects = raycaster.intersectObjects( scene.children );
-  
-      for ( var i = 0; i < intersects.length; i++ ) {
-          console.log( intersects[ i ] ); 
-          /*
-              An intersection has the following properties :
-                  - object : intersected object (THREE.Mesh)
-                  - distance : distance from camera to intersection (number)
-                  - face : intersected face (THREE.Face3)
-                  - faceIndex : intersected face index (number)
-                  - point : intersection point (THREE.Vector3)
-                  - uv : intersection point in the object's UV coordinates (THREE.Vector2)
-          
-      }*/
-  
-  }
-  function onMouseMoveMarker(event) {
-    //event.preventDefault();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera);
-    
-    //console.log("ist die Funktion onMouseMove richtig");
-    //console.log("mouse.x ist :"+ mouse.x);
-    intscs = raycaster.intersectObjects(scene.children,true );
-    if (intscs.length > 0) {
-      console.log("ist die funktion onmouse:");
-      let o = intscs[0];
-      poi.copy(o.point);
-      o.object.worldToLocal(poi);
-      setPos(o.faceIndex);
-      o.object.localToWorld(pos);
-      marker.position.copy(pos);
-    }
-  }
-  
-  function setPos(faceIndex) {
-    tp[0].fromBufferAttribute(intscs[0].object.geometry.attributes.position, faceIndex * 3 + 0);
-    tp[1].fromBufferAttribute(intscs[0].object.geometry.attributes.position, faceIndex * 3 + 1);
-    tp[2].fromBufferAttribute(intscs[0].object.geometry.attributes.position, faceIndex * 3 + 2);
-    tri.set(tp[0], tp[1], tp[2]);
-    tri.getBarycoord(poi, bc);
-    if (bc.x > bc.y && bc.x > bc.z) {
-      idx = 0;
-    } else if (bc.y > bc.x && bc.y > bc.z) {
-      idx = 1;
-    } else if (bc.z > bc.x && bc.z > bc.y) {
-      idx = 2;
-    }
-    pos.copy(tp[idx]);
-  }
-  
+        // raycasterp.object.localToWorld(tempvertex );
+        // marker.position.copy(tempvertex);
 
+      }
+  
+  }
+  
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
     const width = canvas.clientWidth;
@@ -266,12 +140,10 @@ function raycast ( e ) {
     requestAnimationFrame(render);
   }
 
-  document.addEventListener('click',onMouseMove );
- 
-  // renderer.domElement.addEventListener("mousemove", onMouseMove,false);
+  document.addEventListener('click', raycast );
 
   requestAnimationFrame(render);
-
+ // diese Funktion erlaubt mir die seite responsiv zu machen
   window.addEventListener('resize', ()=>{
     camera.aspect= window.innerWidth/window.innerHeight ;
     camera.updateProjectionMatrix();
